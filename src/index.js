3ab8b9f1d1a21c3d65fe914aa52265f282ae3fe3
Heidi Vice <82731725+HeidiVice@users.noreply.github.com>
2025-07-15 09:58:56 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -281,11 +281,7 @@ function play(){
  * Note: Simply uncommenting `setDuration(10);` and `showUp();` is not enough. To make the game work, ensure all necessary functions listed above are called to initialize the score, timer, event listeners, and mole appearances. 
 */
 function startGame(){
-  clearScore();
-  stopGame();   //optional
   setDuration(10);
-  setEventListeners();
-  startTimer();
   showUp();
   return "game started";
 }
