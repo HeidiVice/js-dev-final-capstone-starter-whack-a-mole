19511be1c7a5171e58a1073cd3a5fffb34bb8460
Heidi Vice <82731725+HeidiVice@users.noreply.github.com>
2025-07-18 07:04:53 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -33,6 +33,30 @@ function chooseHole(holes) {
   return hole;
 }
 
+//AUDIO PORTION START
+
+const audioHit = new Audio("https://github.com/gabrielsanchez/erddiagram/blob/main/hit.mp3?raw=true");
+const song = new Audio("https://github.com/gabrielsanchez/erddiagram/blob/main/molesong.mp3?raw=true");
+
+function playAudio(audioObject) {
+  audioObject.play();
+}
+
+function loopAudio(audioObject) {
+  audioObject.loop = true;
+  playAudio(audioObject);
+}
+
+function stopAudio(audioObject) {
+  audioObject.pause();
+}
+
+function play(){
+  playAudio(song);
+}
+
+//AUDIO PORTION END
+
 function toggleVisibility(hole) {
   hole.classList.toggle('show');
   return hole;
